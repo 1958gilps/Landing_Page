947e0cddcda69281dfb3a51b0583b1d1863f4766
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-02-26 13:33:12 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -45,14 +45,33 @@ for (let i = 0; i < navSection.length; i++) {
     
     const newList = document.createElement('li');
     
-    newList.appendChild(linkAnchor)
-    navList.appendChild(newList)
+    newList.appendChild(linkAnchor);
+    navList.appendChild(newList);
+    linkAnchor.addEventListener('click', addScroll );
 }
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function addScroll(event){
+    console.log(event);
+    event.preventDefault();
+    // get the anchor clicked on
+    let a = event.target; 
+    // get the href of the anchor
+    let href = a.getAttribute('href');
+    console.log(href);
+    // get the section from the href
+    let section = document.querySelector(href);
+    console.log(section);
+    // get the top of that section href.section
+    let sectionTop = section.offsetTop;
+    scrollTo({
+        top: sectionTop,
+        behavior: 'smooth'
+    })
+    console.log('Howdy !!! ');
+}
 
 
 
