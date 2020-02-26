dcb83a381a84f91ebea7da609ba21b1035b4b87f
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-02-26 15:07:12 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -27,6 +27,13 @@ const navSection = document.getElementsByTagName('section');
  * 
 */
 
+function elementInViewport(element){
+	// if element is currently on screen, return true
+  	// if it isn't, return false
+  	const rect = element.getBoundingClientRect(),
+          viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top > viewHeight);
+}
 
 /**
  * End Helper Functions
@@ -35,18 +42,26 @@ const navSection = document.getElementsByTagName('section');
 */
 
 // build the nav
+    // find each nav section
 for (let i = 0; i < navSection.length; i++) {
+    // # of nav section
     const eachSection = navSection[i];
+    // get the hyperlink names
     const linkName = (eachSection.querySelector('h2')).textContent;
+    // build the anchor tag
     const linkAnchor = document.createElement('a');
-    
+    // reference each anchor tag
     linkAnchor.href = `#${eachSection.id}`;
+    // give the hyperlinks a name
     linkAnchor.textContent = linkName;
     
+    // build the unordered list
     const newList = document.createElement('li');
     
+    // build and append the anchor(s)
     newList.appendChild(linkAnchor);
     navList.appendChild(newList);
+    // add the scroll function
     linkAnchor.addEventListener('click', addScroll );
 }
 // Add class 'active' to section when near top of viewport
@@ -54,26 +69,20 @@ for (let i = 0; i < navSection.length; i++) {
 
 // Scroll to anchor ID using scrollTO event
 function addScroll(event){
-    console.log(event);
     event.preventDefault();
     // get the anchor clicked on
     let a = event.target; 
     // get the href of the anchor
     let href = a.getAttribute('href');
-    console.log(href);
     // get the section from the href
     let section = document.querySelector(href);
-    console.log(section);
     // get the top of that section href.section
     let sectionTop = section.offsetTop;
     scrollTo({
         top: sectionTop,
         behavior: 'smooth'
     })
-    console.log('Howdy !!! ');
 }
-
-
 
 /**
  * End Main Functions
@@ -87,32 +96,38 @@ function addScroll(event){
 
 // Set sections as active
 
-function elementInViewport(element){
-	//if element is currently on screen, return true
-  	//if it isn't, return false
-  	const rect = element.getBoundingClientRect(),
-          viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top > viewHeight);
-}
 
-function sectionsInViewport(){
-  	//get all sections
+// use line 98 or 99 but not both...
+// function sectionsInViewport(){
+const sectionsInViewport = () => {
+  	// get all sections
   	const sections = document.querySelectorAll("section");
-	//loop through each section, one at a time
+	// loop through each section, one at a time
   	for (let i=0; i<sections.length; i++){
-      	//get current section from loop
+      	// get current section from loop
       	let section = sections[i];
-      	//is section in viewport?
+      	// is section in viewport?
       	if (elementInViewport(section)){
-            //section is in viewport, add "active" class to it (using classlist)
+            // section is in viewport, add "active" class to it (using classlist)
           	section.classList.add("inViewport");
         }
       	else {
-            //section is NOT in viewport, remove "active" class to it (using classlist)
+            // section is NOT in viewport, remove "active" class to it (using classlist)
           	section.classList.remove("inViewport");
         }
     }
-}
+};
 
-//listen for scroll events and call sectionsInViewport function
+// listen for scroll events and call sectionsInViewport function
 window.addEventListener("scroll", sectionsInViewport);
+window.addEventListener("scroll", showHideNavigation);
+
+let debouncer;
+
+function showHideNavigation(){
+    const header = document.querySelector('header');
+    header.classList.remove('notscroll');
+    debouncer = setTimeout(()=>{
+        header.classList.add('notscroll');
+    }, 10000);
+}
