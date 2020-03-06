b999b102cc8905828048669aada5dcb3a0d6044b
1958gilps <48761839+1958gilps@users.noreply.github.com>
2020-03-05 21:56:00 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-/**
- * 
+/*
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
@@ -17,23 +16,14 @@
  * Define Global Variables
  * 
 */
-// find navbar__List and assign it to navList
-const navList = document.querySelector('#navbar__list');
-//find all the section(s) with lots of info and assign to navSection
-const navSection = document.getElementsByTagName('section');
-//const myFragment = document.createDocumentFragment();
+const navList = document.querySelector('#navbar__list'); // find navbar__List and assign it to navList
+const navSection = document.getElementsByTagName('section'); //find all the section(s) with lots of info and assign to navSection
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function elementInViewport(element){
-	// if element is currently on screen, return true
-  	// if it isn't, return false
-  	const rect = element.getBoundingClientRect(),
-          viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top > viewHeight);
-}
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -41,40 +31,29 @@ function elementInViewport(element){
 */
 
 // build the nav
-    // discover how many sections
-for (let i = 0; i < navSection.length; i++) {
-    console.log(navSection[i].id);
-    // discover section(s) info and assign to eachSection
-    const eachSection = navSection[i];
-    // get the section names
-    const linkName = (eachSection.querySelector('h2')).textContent;
-    // build the anchor tag
-    const linkAnchor = document.createElement('a');
-    // discover each anchor url
-    linkAnchor.href = `#${eachSection.id}`;
-    // give the hyperlinks a name
-    linkAnchor.textContent = linkName;
-    // build the unordered list
-    const newList = document.createElement('li');
-    // build and append the anchor(s)
-    newList.appendChild(linkAnchor);
+    for (let i = 0; i < navSection.length; i++) { // discover how many sections
+    const eachSection = navSection[i]; // discover section(s) info and assign to eachSection
+    const linkName = (eachSection.querySelector('h2')).textContent; // get the section names
+    const linkAnchor = document.createElement('a'); // build the anchor tag
+    linkAnchor.href = `#${eachSection.id}`; // discover each anchor url
+    linkAnchor.textContent = linkName; // give the hyperlinks a name
+    const newList = document.createElement('li'); // build the unordered list
+    const newItem = 'nav-link' + i; // add ID
+    newList.setAttribute('id', newItem); // set new ID
+    newList.setAttribute('class', 'menu__link'); // link the ccs
+    newList.appendChild(linkAnchor); // build and append the anchor(s)
     navList.appendChild(newList);
-    // add the scroll function
-    linkAnchor.addEventListener('click', addScroll );
+    linkAnchor.addEventListener('click', addScroll ); // add the scroll function
 }
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
 function addScroll(event){
-    event.preventDefault();
-    // get the anchor clicked on
-    let a = event.target;
-    // get the href of the anchor
-    let href = a.getAttribute('href');
-    // get the section from the href
-    let section = document.querySelector(href);
-    // get the top of that section href.section
-    let scrollTop = section.offsetTop;
+    event.preventDefault(); // prevent the click from completing
+    let a = event.target; // get the anchor clicked on
+    let href = a.getAttribute('href'); // get the href of the anchor
+    let section = document.querySelector(href); // get the section from the href
+    let scrollTop = section.offsetTop; // get the top of that section href.section
 ;
     scrollTo({
         top: scrollTop,
@@ -95,52 +74,36 @@ function addScroll(event){
 
 // highlight navbar
 
-const navbarHighlight = () => {
-    // get all links
-    const links = document.querySelectorAll("li");
-  // loop through each li, one at a time
-    for (let i=0; i<links.length; i++){
-        // get current li from loop
-        let link = links[i];
-        // add classList to each li?
-        link.classList.add(navSection[i].id);
+const sectionsInViewport = () => {
+  	const sections = document.querySelectorAll("section"); // get all sections
+  	for (let i=0; i<sections.length; i++){ // loop through each section, one at a time
+      	let section = sections[i]; // get current section from loop
+      	if (elementInViewport(section)){ // is section in viewport?
+            section.classList.add("inViewport");  // section is in viewport, add "active" class to it (using classlist)
+        }
+      	else {
+            section.classList.remove("inViewport"); // section is NOT in viewport, remove "active" class to it (using classlist)
+        }
     }
 };
 
-const sectionsInViewport = () => {
-  	// get all sections
-  	const sections = document.querySelectorAll("section");
-	// loop through each section, one at a time
-  	for (let i=0; i<sections.length; i++){
-      	// get current section from loop
-      	let section = sections[i];
-      	// is section in viewport?
-      	if (elementInViewport(section)){
-            // section is in viewport, add "active" class to it (using classlist)
-            section.classList.add("inViewport");
-        }
-      	else {
-            // section is NOT in viewport, remove "active" class to it (using classlist)
-            section.classList.remove("inViewport");
-        }
-    }
-};
+function elementInViewport(element){
+	// if element is currently on screen, return true
+  	// if it isn't, return false
+  	const rect = element.getBoundingClientRect(),
+          viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top > viewHeight);
+}
 
 // listen for scroll events and call sectionsInViewport function
 window.addEventListener("scroll", sectionsInViewport);
-window.addEventListener("scroll", navbarHighlight);
-//window.addEventListener("scroll", showHideNavigation);
 
-/*
-window.addEventListener("click", showHideNavigation);
 
 let debouncer;
-
 function showHideNavigation(){
     const header = document.querySelector('header');
     header.classList.remove('notscroll');
     debouncer = setTimeout(()=>{
         header.classList.add('notscroll');
-    }, 20000);
+    }, 2000);
 }
-*/
